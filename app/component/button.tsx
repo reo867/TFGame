6b5e0f51b702b98bb4822f5b25ea27040a9785b6
miwa reou <reou@miwanoMacBook-Pro.local>
2025-05-21 19:44:39 +0900

    iconを変えてみた

--- app/component/button.tsx
+++ app/component/button.tsx
@@ -1,28 +1,31 @@
 "use client";
-import { useState } from "react";
+import { ReactElement, useState } from "react";
 import styles from "./button.module.css";
-
+import { FaRegCircle } from "react-icons/fa6"
+import { RxCross1 } from "react-icons/rx";
 interface ButtonProps {
   team: boolean;
   changeName: (newValue: boolean) => void;
   // team: str
   // ing | boolean;
 }
-
+const iconMaru = <FaRegCircle/>
+const iconBatsu = <RxCross1 />
 const Button = (props:ButtonProps) => {
   // console.log(team);
   let newValue: string | boolean = "";
-    const [marubatsu, setMarubatsu] = useState("");
+    const [marubatsu, setMarubatsu] = useState<ReactElement>();
     const shiraberu = () => {
       console.log(props.team);
         if (props.team === true) {
-          setMarubatsu("まる")
+          setMarubatsu(iconMaru)
           // value = marubatsu;
           newValue = props.team;
           return props.changeName(newValue);
         } else {
+
           // console.log(team);
-          setMarubatsu("ばつ")
+          setMarubatsu(iconBatsu)
           // value = marubatsu;
           newValue = props.team;
           return props.changeName(newValue);
@@ -34,7 +37,7 @@ const Button = (props:ButtonProps) => {
           <button onClick={shiraberu} className={styles.botan}>
                 <span className={styles.tyousei}>{marubatsu}</span>
           </button>
-          
+
         </>
       );
     };
