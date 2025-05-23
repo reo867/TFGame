26c6003b48ea1035023f90664fdc07d97c1c29a8
miwa reou <reou@miwanoMacBook-Pro.local>
2025-05-23 10:10:47 +0900

    とりあえず勝負判定が出るようになった

--- app/page.tsx
+++ app/page.tsx
@@ -24,13 +24,13 @@ export default function Home() {
   // }
   // [123, 456, 789, 147, 258, 369, 159, 357]
   const kotae01: number[] = [1, 2, 3];
-  // const kotae02:number[] = [4, 5, 6]
-  // const kotae03:number[] = [7, 8, 9]
-  // const kotae04:number[] = [1, 4, 7]
-  // const kotae05:number[] = [2, 5, 8]
-  // const kotae06:number[] = [3, 6, 9]
-  // const kotae07:number[] = [1, 5, 9]
-  // const kotae08:number[] = [3, 5, 7]
+  const kotae02: number[] = [4, 5, 6]
+  const kotae03: number[] = [7, 8, 9]
+  const kotae04: number[] = [1, 4, 7]
+  const kotae05: number[] = [2, 5, 8]
+  const kotae06: number[] = [3, 6, 9]
+  const kotae07: number[] = [1, 5, 9]
+  const kotae08: number[] = [3, 5, 7]
   function hasPartialMatch(kotae01: number[], kachi: number[]): boolean {
     return kotae01.every((element) => kachi.includes(element));
   }
@@ -56,6 +56,27 @@ export default function Home() {
     if (hasPartialMatch(kotae01, playerA)) {
       return setResult("playerAの勝ち!!");
     }
+    if (hasPartialMatch(kotae02, playerA)){
+      return setResult("playerAの勝ち")
+    }
+    if (hasPartialMatch(kotae03, playerA)) {
+      return setResult("playerAの勝ち")
+    }
+    if (hasPartialMatch(kotae04, playerA)) {
+      return setResult("playerAの勝ち")
+    }
+    if (hasPartialMatch(kotae05, playerA)) {
+      return setResult("playerAの勝ち")
+    }
+    if (hasPartialMatch(kotae06, playerA)) {
+      return setResult("playerAの勝ち")
+    }
+    if (hasPartialMatch(kotae07, playerA)) {
+      return setResult("playerAの勝ち")
+    }
+    if (hasPartialMatch(kotae08, playerA)) {
+      return setResult("playerAの勝ち")
+    }
   };
   const decideLose = (lose: number) => {
     // const make: number[] = [];
@@ -64,6 +85,28 @@ export default function Home() {
     if (hasPartialMatch(kotae01, playerB)) {
       return setResult("playerBの勝ち!!");
     }
+    if (hasPartialMatch(kotae02, playerB)){
+      return setResult("playerBの勝ち")
+    }
+    if (hasPartialMatch(kotae03, playerB)) {
+      return setResult("playerBの勝ち")
+    }
+    if (hasPartialMatch(kotae04, playerB)) {
+      return setResult("playerBの勝ち")
+    }
+    if (hasPartialMatch(kotae05, playerB)) {
+      return setResult("playerBの勝ち")
+    }
+    if (hasPartialMatch(kotae06, playerB)) {
+      return setResult("playerBの勝ち")
+    }
+    if (hasPartialMatch(kotae07, playerB)) {
+      return setResult("playerBの勝ち")
+    }
+    if (hasPartialMatch(kotae08, playerB)) {
+      return setResult("playerBの勝ち")
+    }
+
   };
 
   return (
